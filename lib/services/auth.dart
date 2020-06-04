45948f8c271d8975c05c766e15255f5ab49347f2
Your Name <matt-picard@hotmail.fr>
2020-06-04 13:09:26 +0200

    Authbase

---
+++ lib/services/auth.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthServices{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Annonymous
+  Future signInAnon() async {
+    try{
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+  //login Email + password
+  //Register Email + password
+  //logout
+}
