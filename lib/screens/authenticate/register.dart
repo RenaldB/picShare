6c0e2e78cb65ca0161eed1388fe54c72a9295459
Your Name <matt-picard@hotmail.fr>
2020-06-08 14:07:00 +0200

    aauuth

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -6,16 +6,14 @@ import 'package:flutter/material.dart';
 import 'package:picshare/shared/loading.dart';
 
 class Register extends StatefulWidget {
-
   final Function toggleView;
-  Register({ this.toggleView });
+  Register({this.toggleView});
 
   @override
   _RegisterState createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
-
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
   String error = '';
@@ -27,78 +25,86 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return loading ? Loading() : Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/astronaut-minimalist-ss.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        padding: EdgeInsets.symmetric(horizontal: 25.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Inscription",
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-              ),
-              RoundedInputField(
-                hintText: "Adresse e-mail",
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              RoundedPasswordField(
-                hintText: "Adresse e-mail",
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              RoundedButton(
-                text: "S'INSCRIRE",
-                fontsize: 20,
-                press: () async {
-                  if(_formKey.currentState.validate()){
-                    setState(() => loading = true);
-                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
-                    if(result == null) {
-                      setState(() {
-                        loading = false;
-                        error = "L'adresse email n'est pas valide";
-                      });
-                    }
-                  }
-                },
-              ),
-              RoundedButton(
-                text: "Se connecter",
-                fontsize: 15,
-                press: () => widget.toggleView(),
-              ),
-              FlatButton(
-                child: Text(
-                  'Continuer sans se connecter',
-                  style: TextStyle(fontSize: 9)
+    return loading
+        ? Loading()
+        : Scaffold(
+            body: SingleChildScrollView(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image:
+                        AssetImage("assets/images/astronaut-minimalist-ss.jpg"),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                onPressed: () async {
-                  _auth.signInAnon();
-                },
+                padding: EdgeInsets.symmetric(horizontal: 25.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Inscription",
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 40),
+                      ),
+                      RoundedInputField(
+                        hintText: "Adresse e-mail",
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        },
+                      ),
+                      RoundedPasswordField(
+                        hintText: "Adresse e-mail",
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      RoundedButton(
+                        text: "S'INSCRIRE",
+                        fontsize: 20,
+                        press: () async {
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .registerWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() {
+                                loading = false;
+                                error = "L'adresse email n'est pas valide";
+                              });
+                            }
+                          }
+                        },
+                      ),
+                      RoundedButton(
+                        text: "Se connecter",
+                        fontsize: 15,
+                        press: () => widget.toggleView(),
+                      ),
+                      FlatButton(
+                        child: Text('Continuer sans se connecter',
+                            style: TextStyle(fontSize: 9)),
+                        onPressed: () async {
+                          _auth.signInAnon();
+                        },
+                      ),
+                      Text(
+                        error,
+                        style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
