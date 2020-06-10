0e913321492de139d5478bc4058f98464ca002f4
Akey <renaldbooz@live.fr>
2020-06-10 17:23:12 +0200

    petite modif

--- lib/screens/components/mainButton.dart
+++ lib/screens/components/mainButton.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'dart:io';
-import 'package:image_cropper/image_cropper.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:picshare/screens/components/pageUpload.dart';
 
 class FancyFab extends StatefulWidget {
@@ -28,41 +24,6 @@ class _FancyFabState extends State<FancyFab>
   double _fabHeight = 56.0;
   String typeButton;
 
- /// Active image file
-  File _imageFile;
-
-  /// Cropper plugin
-  Future<void> _cropImage() async {
-    File cropped = await ImageCropper.cropImage(
-        sourcePath: _imageFile.path,
-        // ratioX: 1.0,
-        // ratioY: 1.0,
-        // maxWidth: 512,
-        // maxHeight: 512,
-      );
-
-    setState(() {
-      _imageFile = cropped ?? _imageFile;
-    });
-  }
-
-  /// Select an image via gallery or camera
-  Future<void> _pickImage(ImageSource source) async {
-    final _picker = ImagePicker();
-    PickedFile selected = await _picker.getImage(source: source);
-
-    setState(() {
-      if(selected.path.isNotEmpty)
-        _imageFile =File(selected.path);
-    });
-    
-  }
-
-  /// Remove image
-  void _clear() {
-    setState(() => _imageFile = null);
-  }
-
   @override
   initState() {
     _animationController =
@@ -73,13 +34,13 @@ class _FancyFabState extends State<FancyFab>
     _animateIcon =
         Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
     _buttonColor = ColorTween(
-      begin: Colors.blue,
+      //begin: Colors.blue,
       end: Colors.red,
     ).animate(CurvedAnimation(
       parent: _animationController,
       curve: Interval(
         0.00,
-        1.00,
+        0.5,
         curve: Curves.linear,
       ),
     ));
@@ -90,7 +51,7 @@ class _FancyFabState extends State<FancyFab>
       parent: _animationController,
       curve: Interval(
         0.0,
-        0.75,
+        0.5,
         curve: _curve,
       ),
     ));
