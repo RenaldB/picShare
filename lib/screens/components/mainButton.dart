6a1b47bdf4f5da12e15baa5f005342c545f87042
Akey <renaldbooz@live.fr>
2020-06-10 14:29:11 +0200

    MainButton mdr

---
+++ lib/screens/components/mainButton.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+
+import 'dart:io';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:picshare/screens/components/pageUpload.dart';
+
+class FancyFab extends StatefulWidget {
+  final Function() onPressed;
+  final String tooltip;
+  final IconData icon;
+
+  FancyFab({this.onPressed, this.tooltip, this.icon});
+
+  @override
+  _FancyFabState createState() => _FancyFabState();
+}
+
+class _FancyFabState extends State<FancyFab>
+    with SingleTickerProviderStateMixin {
+
+  bool isOpened = false;
+  AnimationController _animationController;
+  Animation<Color> _buttonColor;
+  Animation<double> _animateIcon;
+  Animation<double> _translateButton;
+  Curve _curve = Curves.easeOut;
+  double _fabHeight = 56.0;
+  String typeButton;
+
+ /// Active image file
+  File _imageFile;
+
+  /// Cropper plugin
+  Future<void> _cropImage() async {
+    File cropped = await ImageCropper.cropImage(
+        sourcePath: _imageFile.path,
+        // ratioX: 1.0,
+        // ratioY: 1.0,
+        // maxWidth: 512,
+        // maxHeight: 512,
+      );
+
+    setState(() {
+      _imageFile = cropped ?? _imageFile;
+    });
+  }
+
+  /// Select an image via gallery or camera
+  Future<void> _pickImage(ImageSource source) async {
+    final _picker = ImagePicker();
+    PickedFile selected = await _picker.getImage(source: source);
+
+    setState(() {
+      if(selected.path.isNotEmpty)
+        _imageFile =File(selected.path);
+    });
+    
+  }
+
+  /// Remove image
+  void _clear() {
+    setState(() => _imageFile = null);
+  }
+
+  @override
+  initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+          ..addListener(() {
+            setState(() {});
+          });
+    _animateIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _buttonColor = ColorTween(
+      begin: Colors.blue,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: Curves.linear,
+      ),
+    ));
+    _translateButton = Tween<double>(
+      begin: _fabHeight,
+      end: -14.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.0,
+        0.75,
+        curve: _curve,
+      ),
+    ));
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  animate() {
+    if (!isOpened) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+    isOpened = !isOpened;
+  }
+
+  Widget add() {
+    return Container(
+      child: FloatingActionButton(
+        heroTag: "btn1",
+        onPressed: (){
+          pageUpload("photo");
+        },
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget image() {
+    return Container(
+      child: FloatingActionButton(
+        heroTag: "btn2",
+        onPressed: (){
+          pageUpload("gallery");
+        },
+        tooltip: 'Image',
+        child: Icon(Icons.image),
+      ),
+    );
+  }
+
+  Widget inbox() {
+    return Container(
+      child: FloatingActionButton(
+        heroTag: "btn3",
+        onPressed: null,
+        tooltip: 'Inbox',
+        child: Icon(Icons.inbox),
+      ),
+    );
+  }
+
+  Widget toggle() {
+    return Container(
+      child: FloatingActionButton(
+        heroTag: "btn4",
+        backgroundColor: _buttonColor.value,
+        onPressed: animate,
+        tooltip: 'Toggle',
+        child: AnimatedIcon(
+          icon: AnimatedIcons.menu_close,
+          progress: _animateIcon,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value * 3.0,
+            0.0,
+          ),
+          child: add(),
+        ),
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value * 2.0,
+            0.0,
+          ),
+          child: image(),
+        ),
+        Transform(
+          transform: Matrix4.translationValues(
+            0.0,
+            _translateButton.value,
+            0.0,
+          ),
+          child: inbox(),
+        ),
+        toggle(),
+      ],
+    );
+  }
+
+    void pageUpload(typeButton) {
+    print("Open");
+    Navigator.push(context,
+        new MaterialPageRoute(builder: (BuildContext context) {
+      print('to upload image');
+      return Gallery(text: typeButton,);
+    }));
+  }
+}
---
+++ lib/screens/components/pageUpload.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'uploadImage.dart';
+
+class Gallery extends StatefulWidget {
+  Gallery({Key key, @required this.text}) : super(key: key);
+  final String text;
+
+  @override
+  _Gallery createState() => new _Gallery();
+}
+
+class _Gallery extends State<Gallery> {
+  /// Active image file
+  File _imageFile;
+  
+
+  /// Cropper plugin
+  Future<void> _cropImage() async {
+    File cropped = await ImageCropper.cropImage(
+      sourcePath: _imageFile.path,
+      // ratioX: 1.0,
+      // ratioY: 1.0,
+      // maxWidth: 512,
+      // maxHeight: 512,
+    );
+
+    setState(() {
+      _imageFile = cropped ?? _imageFile;
+    });
+  }
+
+  /// Select an image via gallery or camera
+  Future<void> _pickImage(ImageSource source) async {
+    final _picker = ImagePicker();
+    PickedFile selected = await _picker.getImage(source: source);
+
+    setState(() {
+      if (selected.path.isNotEmpty) _imageFile = File(selected.path);
+    });
+  }
+
+  /// Remove image
+  void _clear() {
+    setState(() => _imageFile = null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_imageFile == null) {
+      print(text);
+      if (text == "gallery") {
+        _pickImage(ImageSource.gallery);
+      }
+      if (text == "photo") {
+        _pickImage(ImageSource.camera);
+      }
+    }
+
+    return Scaffold(
+      // Select an image from the camera or gallery
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.photo_camera),
+              onPressed: () => _pickImage(ImageSource.camera),
+            ),
+            IconButton(
+              icon: Icon(Icons.photo_library),
+              onPressed: () => _pickImage(ImageSource.gallery),
+            ),
+          ],
+        ),
+      ),
+
+      // Preview the image and crop it
+      body: ListView(
+        children: <Widget>[
+          if (_imageFile != null) ...[
+            Image.file(_imageFile),
+            Row(
+              children: <Widget>[
+                FlatButton(
+                  child: Icon(Icons.crop),
+                  onPressed: _cropImage,
+                ),
+                FlatButton(
+                  child: Icon(Icons.refresh),
+                  onPressed: _clear,
+                ),
+              ],
+            ),
+            Uploader(file: _imageFile)
+          ]
+        ],
+      ),
+    );
+  }
+}
